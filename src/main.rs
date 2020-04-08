cd67bbefd4124572b87befbdb70c567fd621e9a3
Youngermaster <jmyoungh@eafit.edu.co>
2020-04-08 13:43:42 -0500

    first code added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rust_gpiozero::*;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new LED attached to Pin 17
+    let led = LED::new(17);
+
+    // Blink the LED 5 times
+    for  _ in  0.. 5{
+       led.on();
+        thread::sleep(Duration::from_secs(1));
+        led.off();
+        thread::sleep(Duration::from_secs(1));
+    }
 }
